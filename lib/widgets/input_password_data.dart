0f1d04b9f6f8924ab795b2be22f1ad7f12a854c9
fad16papa <fad16papa@gmail.com>
2021-05-08 23:18:55 +0800

    Commit changes in Login password screen added obscure for password text field

--- lib/widgets/input_password_data.dart
+++ lib/widgets/input_password_data.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class InputPassword extends StatelessWidget {
+class InputPassword extends StatefulWidget {
   final String hintTextInput;
 
   InputPassword(this.hintTextInput);
+
+  @override
+  _InputPasswordState createState() => _InputPasswordState();
+}
+
+class _InputPasswordState extends State<InputPassword> {
+  bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +18,18 @@ class InputPassword extends StatelessWidget {
       margin: EdgeInsets.all(10),
       padding: EdgeInsets.all(10),
       child: TextField(
+        obscureText: _isObscure,
         decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-          hintText: hintTextInput,
+          hintText: widget.hintTextInput,
+          suffixIcon: IconButton(
+            icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
+            onPressed: () {
+              setState(() {
+                _isObscure = !_isObscure;
+              });
+            },
+          ),
         ),
       ),
     );
