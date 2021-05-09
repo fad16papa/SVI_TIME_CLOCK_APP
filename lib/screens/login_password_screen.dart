5ee9b43b571866c7b159ac90849cfc31f78d843c
fad16papa <fad16papa@gmail.com>
2021-05-09 17:36:20 +0800

    Commit changse

--- lib/screens/login_password_screen.dart
+++ lib/screens/login_password_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:svi_time_clock_app/widgets/button_color.dart';
 import 'package:svi_time_clock_app/widgets/button_plain.dart';
 import 'package:svi_time_clock_app/widgets/button_text.dart';
